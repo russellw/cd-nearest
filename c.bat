5f83da1b8ed90a3ecb3545364e03395b129fde81
Russell Wallace <russell.wallace@gmail.com>
2024-05-16 16:03:51 +0100

    actual cd

---
+++ c.bat
@@ -0,0 +1,9 @@
+@echo off
+:: Run calc-dir.exe and capture its output
+for /f "delims=" %%i in ('nearest.exe') do set "targetDir=%%i"
+
+:: Output the captured directory for debugging
+rem echo Calculated directory: %targetDir%
+
+:: Change to the captured directory
+cd /d "%targetDir%"
